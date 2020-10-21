8ad1f7f257a3ca0c9903c826815f98ff77129145
fkibria <fkibria02@gmail.com>
2020-10-20 23:58:13 -0400

    created the confirm and prompt boxes for each variable

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Assignment Code
+
+//Create arrays for password options 
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var special = '~!@#$^&%*()+=-_[]{}|:;<>?,.';
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  var passwordLength = prompt("How long would you like your password to be? (Minimum 8 characters)");
+
+  var confirmLower = confirm("Would you like your password to include lowercase letters?");
+
+  var confirmUpper = confirm("Would you like your password to include uppercase letters?");
+
+  var confirmSpecial = confirm("Would you like your password to include special characters?");
+
+  var confirmNumbers = confirm("Would you like your password to include numbers?");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
