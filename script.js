4ac7b3d5b057d92149d9932959f293353bd455e8
fkibria <fkibria02@gmail.com>
2020-10-21 01:59:34 -0400

    adding for loop and return for password

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var chosenPrompts ='';
 
   var passwordLength = prompt("How long would you like your password to be? (Minimum 8 characters)");
-
-  while(passwordLength < 8 || passwordLength > 128) {
+  
+  while((passwordLength < 8 || passwordLength > 128) && typeof passwordLength !== "number") { 
     alert("Password must be between 8 and 128 characters!");
     passwordLength = prompt("How long would you like your password to be? (Minimum 8 characters)");
   } 
@@ -21,33 +22,38 @@ function writePassword() {
   if(confirmLower) {
     chosenPrompts += lower;
     console.log(chosenPrompts);
-}
+  }
 
   var confirmUpper = confirm("Would you like your password to include uppercase letters?");
 
   if(confirmUpper) {
     chosenPrompts += lower.toUpperCase();
     console.log(chosenPrompts);
-}
+  }
 
   var confirmSpecial = confirm("Would you like your password to include special characters?");
 
   if(confirmSpecial) {
     chosenPrompts += special;
     console.log(chosenPrompts);
-}
+  }
 
   var confirmNumbers = confirm("Would you like your password to include numbers?");
 
   if(confirmNumbers) {
     chosenPrompts += Math.floor(Math.random() * 1000000000);
     console.log(chosenPrompts);
-}
+  }
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = "";
 
-  passwordText.value = password;
+   for (var i = 0; i < passwordLength; ++i) {
+    password += chosenPrompts.charAt(Math.floor(Math.random() * chosenPrompts.length));
+  }
+console.log(password);
+document.getElementById("password").innerHTML = password;
+return password;
+
 
 }
 
